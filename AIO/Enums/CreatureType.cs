f8395a91faff6361dbd770e19896a514b442b547
Nietzschex <hellocpp@163.com>
2026-01-09 09:13:20 +0800

    fix

---
+++ AIO/Enums/CreatureType.cs
@@ -0,0 +1,21 @@
+namespace AIO.Enums
+{
+    public enum CreatureType
+    {
+        None,
+        Beast,
+        Demon,
+        Dragonkin,
+        Elemental,
+        Giant,
+        Humanoid,
+        Mechanical,
+        Undead,
+        Critter,
+        NonCombatPet,
+        GasCloud,
+        WildPet,
+        Aberration,
+        Totem
+    }
+}
